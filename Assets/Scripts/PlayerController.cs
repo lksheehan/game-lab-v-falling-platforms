c333a772c4178f732652fe29b216470d01a62b50
lksheehan <55460848+lksheehan@users.noreply.github.com>
2020-09-02 10:36:10 -0400

    added falling platforms

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+    }
+}
